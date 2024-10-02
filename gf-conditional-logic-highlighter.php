0eece6e2cb3c9275fb30e067fc39c42c959eca9a
fg@webdevels.de <fg@webdevels.de>
2024-10-02 02:01:50 +0200

    removed uri info

--- gf-conditional-logic-highlighter.php
+++ gf-conditional-logic-highlighter.php
@@ -10,7 +10,6 @@
  * Domain Path: /languages
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- * GitHub Plugin URI: https://github.com/WebDevels-de/gf-conditional-logic-highlighter
  */
 
 // If this file is called directly, abort.
